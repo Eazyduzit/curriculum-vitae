6ecbc9b6ec068425649657f8cf3b836e2462f623
Eazyduzit <Eazyduzit@users.noreply.github.com>
2017-03-14 15:13:02 +0100

    added tables to erfaring + show hide function

---
+++ script/CV.js
@@ -0,0 +1,8 @@
+function showHide() {
+    var myTable = document.getElementById('tableDiv');
+    if (myTable.style.visibility === 'hidden') {
+        myTable.style.visibility = 'visible';
+    } else {
+        myTable.style.visibility = 'hidden';
+    }
+}
